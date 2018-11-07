5967b0f55065451527647544f612a3228030b490
Joseph B. Zambon <jbzambon@ncsu.edu>
2018-11-07 04:41:45 -0500

    Update wrfout_to_json.py
    Add fixes and time-variant loop

--- wrfout_to_json.py
+++ wrfout_to_json.py
@@ -12,7 +12,7 @@
 # conda install -c conda-forge proj4
 # jupyter notebook --profile=nbserver
 
-%pylab inline
+# %pylab inline
 from netCDF4 import Dataset
 import numpy as np
 from scipy.interpolate import griddata
@@ -21,19 +21,22 @@ from mpl_toolkits.basemap import Basemap
 
 from scipy.interpolate import griddata
 
-wrfout = '/home/ubuntu/cnaps_json/20180925.nc'
-outfile = '/home/ubuntu/cnaps_json/current-wind-surface-level-gfs-0.25.json'
+wrfout = '/home/ec2-user/cnaps2json/20181106.nc'
+outdir = '/home/ec2-user/cnaps2json/json/'
 dx = 0.1    # Delta longitude
 dy = 0.1    # Delta latitude
 
 wrfout = Dataset(wrfout)
-print(wrfout.file_format)
-print(wrfout.dimensions.keys())
+# print(wrfout.file_format)
+# print(wrfout.dimensions.keys())
 
 time = np.array(wrfout['DateTime'])
-print(np.array(wrfout['DateTime']))
+# print(np.array(wrfout['DateTime']))
 
-print(wrfout.variables.keys())
+# print(wrfout.variables.keys())
+
+[d_lon,d_lat] = np.meshgrid(np.arange(min(lon[0,:])-1e-2, max(lon[0,:])+1e-2, dx),\
+                           np.arange(min(lat[:,0])-1e-2, max(lat[:,0])+1e-2, dy))
 
 lat = np.array(wrfout['lat'])
 #lat = np.squeeze(lat[0,:])
@@ -43,200 +46,202 @@ time = np.array(wrfout['DateTime'])
 u_10m_tr = np.array(wrfout['u_10m_tr'])
 v_10m_tr = np.array(wrfout['v_10m_tr'])
 
-d_u_10m_tr = griddata((lon.ravel(), lat.ravel()),\
-                      np.squeeze(u_10m_tr[0,:,:]).ravel(),\
-                      (d_lon, d_lat), method='nearest')
-d_v_10m_tr = griddata((lon.ravel(), lat.ravel()),\
-                      np.squeeze(v_10m_tr[0,:,:]).ravel(),\
-                      (d_lon, d_lat), method='nearest')
+for t in range (0,size(time)):
 
-print(d_u_10m_tr)
+    d_u_10m_tr = griddata((lon.ravel(), lat.ravel()),\
+                          np.squeeze(u_10m_tr[t,:,:]).ravel(),\
+                          (d_lon, d_lat), method='nearest')
+    d_v_10m_tr = griddata((lon.ravel(), lat.ravel()),\
+                          np.squeeze(v_10m_tr[t,:,:]).ravel(),\
+                          (d_lon, d_lat), method='nearest')
 
-figsize(25,15)
-map = Basemap(projection='merc',
-      resolution='l',lat_0=((np.max(lat)-np.min(lat))/2),
-      lon_0=((np.max(lon)-np.min(lon))/2),
-      llcrnrlon=np.min(lon),llcrnrlat=np.min(lat),
-      urcrnrlon=np.max(lon),urcrnrlat=np.max(lat))
-map.drawcoastlines()
-map.drawcountries()
-map.drawstates()
-map.pcolormesh(d_lon,d_lat,d_u_10m_tr[:,:],vmin=0,vmax=10,latlon='true')
+    # figsize(25,15)
+    # map = Basemap(projection='merc',
+    #       resolution='l',lat_0=((np.max(lat)-np.min(lat))/2),
+    #       lon_0=((np.max(lon)-np.min(lon))/2),
+    #       llcrnrlon=np.min(lon),llcrnrlat=np.min(lat),
+    #       urcrnrlon=np.max(lon),urcrnrlat=np.max(lat))
+    # map.drawcoastlines()
+    # map.drawcountries()
+    # map.drawstates()
+    # map.pcolormesh(d_lon,d_lat,d_u_10m_tr[:,:],vmin=0,vmax=10,latlon='true')
 
-figsize(25,15)
-map = Basemap(projection='merc',
-      resolution='l',lat_0=((np.max(lat)-np.min(lat))/2),
-      lon_0=((np.max(lon)-np.min(lon))/2),
-      llcrnrlon=np.min(lon),llcrnrlat=np.min(lat),
-      urcrnrlon=np.max(lon),urcrnrlat=np.max(lat))
-map.drawcoastlines()
-map.drawcountries()
-map.drawstates()
-map.pcolormesh(d_lon,d_lat,d_v_10m_tr[:,:],vmin=0,vmax=10,latlon='true')
+    # figsize(25,15)
+    # map = Basemap(projection='merc',
+    #       resolution='l',lat_0=((np.max(lat)-np.min(lat))/2),
+    #       lon_0=((np.max(lon)-np.min(lon))/2),
+    #       llcrnrlon=np.min(lon),llcrnrlat=np.min(lat),
+    #       urcrnrlon=np.max(lon),urcrnrlat=np.max(lat))
+    # map.drawcoastlines()
+    # map.drawcountries()
+    # map.drawstates()
+    # map.pcolormesh(d_lon,d_lat,d_v_10m_tr[:,:],vmin=0,vmax=10,latlon='true')
 
-figsize(25,15)
-map = Basemap(projection='merc',
-      resolution='l',lat_0=((np.max(lat)-np.min(lat))/2),
-      lon_0=((np.max(lon)-np.min(lon))/2),
-      llcrnrlon=np.min(lon),llcrnrlat=np.min(lat),
-      urcrnrlon=np.max(lon),urcrnrlat=np.max(lat))
-map.drawcoastlines()
-map.drawcountries()
-map.drawstates()
-map.pcolormesh(d_lon,d_lat,sqrt(d_u_10m_tr[:,:]**2 +\
-                                d_u_10m_tr[:,:]**2),\
-               vmin=0,vmax=10,latlon='true')
+    # figsize(25,15)
+    # map = Basemap(projection='merc',
+    #       resolution='l',lat_0=((np.max(lat)-np.min(lat))/2),
+    #       lon_0=((np.max(lon)-np.min(lon))/2),
+    #       llcrnrlon=np.min(lon),llcrnrlat=np.min(lat),
+    #       urcrnrlon=np.max(lon),urcrnrlat=np.max(lat))
+    # map.drawcoastlines()
+    # map.drawcountries()
+    # map.drawstates()
+    # map.pcolormesh(d_lon,d_lat,sqrt(d_u_10m_tr[:,:]**2 +\
+    #                                 d_u_10m_tr[:,:]**2),\
+    #                vmin=0,vmax=10,latlon='true')
 
-d_u_10m_tr = np.flipud(d_u_10m_tr)
-d_v_10m_tr = np.flipud(d_v_10m_tr)
+    d_u_10m_tr = np.flipud(d_u_10m_tr)
+    d_v_10m_tr = np.flipud(d_v_10m_tr)
 
-print(time[0])
+    outfile = outdir + str(time[t]) + '_sfc_wind.json'
+    
+    print(outfile)
 
-json_out = open(outfile,'w')
-curr_time = str(time[0])
-yyyy = curr_time[0:4]
-mm = curr_time[4:6]
-dd = curr_time[6:8]
-hh = curr_time[8:10]
-points = np.shape(d_lon[0])[0] * np.shape(d_lat[0])[0]
-nx = np.shape(d_lon[0,:])[0]
-ny = np.shape(d_lat[:,0])[0]
+    json_out = open(outfile,'w')
+    curr_time = str(time[t])
+    yyyy = curr_time[0:4]
+    mm = curr_time[4:6]
+    dd = curr_time[6:8]
+    hh = curr_time[8:10]
+    points = np.shape(d_lon[0])[0] * np.shape(d_lat[0])[0]
+    nx = np.shape(d_lon[0,:])[0]
+    ny = np.shape(d_lat[:,0])[0]
 
-# U-component of surface wind
-outstr = str("[ \n\
-    {\n\
-        \"header\":{\n\
-            \"discipline\":0,\n\
-            \"disciplineName\":\"Meteorological products\",\n\
-            \"gribEdition\":2,\n\
-            \"gribLength\":791507,\n\
-            \"center\":8,\n\
-            \"centerName\":\"NCSU CNAPS\",\n\
-            \"subcenter\":0,\n\
-            \"refTime\":\"" + str(yyyy) + "-" + str(mm) + "-" + str(dd) + "T" + str(hh) +":00:00.000Z\",\n\
-            \"significanceOfRT\":1,\n\
-            \"significanceOfRTName\":\"Start of forecast\",\n\
-            \"productStatus\":0,\n\
-            \"productStatusName\":\"Operational products\",\n\
-            \"productType\":1,\n\
-            \"productTypeName\":\"Forecast products\",\n\
-            \"productDefinitionTemplate\":0,\n\
-            \"productDefinitionTemplateName\":\"Analysis/forecast at horizontal level/layer at a point in time\",\n\
-            \"parameterCategory\":2,\n\
-            \"parameterCategoryName\":\"Momentum\",\n\
-            \"parameterNumber\":2,\n\
-            \"parameterNumberName\":\"U-component_of_wind\",\n\
-            \"parameterUnit\":\"m.s-1\",\n\
-            \"genProcessType\":2,\n\
-            \"genProcessTypeName\":\"Forecast\",\n\
-            \"forecastTime\":0,\n\
-            \"surface1Type\":103,\n\
-            \"surface1TypeName\":\"Specified height level above ground\",\n\
-            \"surface1Value\":10.0,\n\
-            \"surface2Type\":255,\n\
-            \"surface2TypeName\":\"Missing\",\n\
-            \"surface2Value\":0.0,\n\
-            \"gridDefinitionTemplate\":0,\n\
-            \"gridDefinitionTemplateName\":\"Latitude_Longitude\",\n\
-            \"numberPoints\":" + str(points) +",\n\
-            \"shape\":6,\n\
-            \"shapeName\":\"Earth spherical with radius of 6,371,229.0 m\",\n\
-            \"gridUnits\":\"degrees\",\n\
-            \"resolution\":48,\n\
-            \"winds\":\"true\",\n\
-            \"scanMode\":0,\n\
-            \"nx\":" + str(nx) + ",\n\
-            \"ny\":" + str(ny) + ",\n\
-            \"basicAngle\":0,\n\
-            \"lo1\":" + str(round(min(d_lon[0,:]),2)) + ",\n\
-            \"la1\":" + str(round(max(d_lat[:,0]),2)) + ",\n\
-            \"lo2\":" + str(round(max(d_lon[0,:]),2)) + ",\n\
-            \"la2\":" + str(round(min(d_lat[:,0]),2)) + ",\n\
-            \"dx\":" + str(dx) + ",\n\
-            \"dy\":" + str(dy) + ",\n\
-        },\
-\n")
+    # U-component of surface wind
+    outstr = str("[ \n\
+        {\n\
+            \"header\":{\n\
+                \"discipline\":0,\n\
+                \"disciplineName\":\"Meteorological products\",\n\
+                \"gribEdition\":2,\n\
+                \"gribLength\":791507,\n\
+                \"center\":8,\n\
+                \"centerName\":\"NCSU CNAPS\",\n\
+                \"subcenter\":0,\n\
+                \"refTime\":\"" + str(yyyy) + "-" + str(mm) + "-" + str(dd) + "T" + str(hh) +":00:00.000Z\",\n\
+                \"significanceOfRT\":1,\n\
+                \"significanceOfRTName\":\"Start of forecast\",\n\
+                \"productStatus\":0,\n\
+                \"productStatusName\":\"Operational products\",\n\
+                \"productType\":1,\n\
+                \"productTypeName\":\"Forecast products\",\n\
+                \"productDefinitionTemplate\":0,\n\
+                \"productDefinitionTemplateName\":\"Analysis/forecast at horizontal level/layer at a point in time\",\n\
+                \"parameterCategory\":2,\n\
+                \"parameterCategoryName\":\"Momentum\",\n\
+                \"parameterNumber\":2,\n\
+                \"parameterNumberName\":\"U-component_of_wind\",\n\
+                \"parameterUnit\":\"m.s-1\",\n\
+                \"genProcessType\":2,\n\
+                \"genProcessTypeName\":\"Forecast\",\n\
+                \"forecastTime\":0,\n\
+                \"surface1Type\":103,\n\
+                \"surface1TypeName\":\"Specified height level above ground\",\n\
+                \"surface1Value\":10.0,\n\
+                \"surface2Type\":255,\n\
+                \"surface2TypeName\":\"Missing\",\n\
+                \"surface2Value\":0.0,\n\
+                \"gridDefinitionTemplate\":0,\n\
+                \"gridDefinitionTemplateName\":\"Latitude_Longitude\",\n\
+                \"numberPoints\":" + str(points) +",\n\
+                \"shape\":6,\n\
+                \"shapeName\":\"Earth spherical with radius of 6,371,229.0 m\",\n\
+                \"gridUnits\":\"degrees\",\n\
+                \"resolution\":48,\n\
+                \"winds\":\"true\",\n\
+                \"scanMode\":0,\n\
+                \"nx\":" + str(nx) + ",\n\
+                \"ny\":" + str(ny) + ",\n\
+                \"basicAngle\":0,\n\
+                \"lo1\":" + str(round(min(d_lon[0,:]),2)) + ",\n\
+                \"la1\":" + str(round(max(d_lat[:,0]),2)) + ",\n\
+                \"lo2\":" + str(round(max(d_lon[0,:]),2)) + ",\n\
+                \"la2\":" + str(round(min(d_lat[:,0]),2)) + ",\n\
+                \"dx\":" + str(dx) + ",\n\
+                \"dy\":" + str(dy) + ",\n\
+            },\
+    \n")
 
-outstr += str("        \"data\":[\n")
-for j in range(0,ny):
-    for i in range(0,nx):
-        if j != (ny-1) or i != (nx-1):
-            outstr += str("            " + str(d_u_10m_tr[j,i]) + ",\n")
-        else:
-            outstr += str("            " + str(d_u_10m_tr[j,i]) + "\n")
+    outstr += str("        \"data\":[\n")
+    for j in range(0,ny):
+        for i in range(0,nx):
+            if j != (ny-1) or i != (nx-1):
+                outstr += str("            " + str(d_u_10m_tr[j,i]) + ",\n")
+            else:
+                outstr += str("            " + str(d_u_10m_tr[j,i]) + "\n")
 
-outstr += str("        ]\n\
-    },\n\
-")
+    outstr += str("        ]\n\
+        },\n\
+    ")
 
-# V-component of surface wind
-            
-outstr += str("    {\n\
-        \"header\":{\n\
-            \"discipline\":0,\n\
-            \"disciplineName\":\"Meteorological products\",\n\
-            \"gribEdition\":2,\n\
-            \"gribLength\":791507,\n\
-            \"center\":8,\n\
-            \"centerName\":\"NCSU CNAPS\",\n\
-            \"subcenter\":0,\n\
-            \"refTime\":\"" + str(yyyy) + "-" + str(mm) + "-" + str(dd) + "T" + str(hh) +":00:00.000Z\",\n\
-            \"significanceOfRT\":1,\n\
-            \"significanceOfRTName\":\"Start of forecast\",\n\
-            \"productStatus\":0,\n\
-            \"productStatusName\":\"Operational products\",\n\
-            \"productType\":1,\n\
-            \"productTypeName\":\"Forecast products\",\n\
-            \"productDefinitionTemplate\":0,\n\
-            \"productDefinitionTemplateName\":\"Analysis/forecast at horizontal level/layer at a point in time\",\n\
-            \"parameterCategory\":2,\n\
-            \"parameterCategoryName\":\"Momentum\",\n\
-            \"parameterNumber\":3,\n\
-            \"parameterNumberName\":\"V-component_of_wind\",\n\
-            \"parameterUnit\":\"m.s-1\",\n\
-            \"genProcessType\":2,\n\
-            \"genProcessTypeName\":\"Forecast\",\n\
-            \"forecastTime\":0,\n\
-            \"surface1Type\":103,\n\
-            \"surface1TypeName\":\"Specified height level above ground\",\n\
-            \"surface1Value\":10.0,\n\
-            \"surface2Type\":255,\n\
-            \"surface2TypeName\":\"Missing\",\n\
-            \"surface2Value\":0.0,\n\
-            \"gridDefinitionTemplate\":0,\n\
-            \"gridDefinitionTemplateName\":\"Latitude_Longitude\",\n\
-            \"numberPoints\":" + str(points) +",\n\
-            \"shape\":6,\n\
-            \"shapeName\":\"Earth spherical with radius of 6,371,229.0 m\",\n\
-            \"gridUnits\":\"degrees\",\n\
-            \"resolution\":48,\n\
-            \"winds\":\"true\",\n\
-            \"scanMode\":0,\n\
-            \"nx\":" + str(nx) + ",\n\
-            \"ny\":" + str(ny) + ",\n\
-            \"basicAngle\":0,\n\
-            \"lo1\":" + str(round(min(d_lon[0,:]),2)) + ",\n\
-            \"la1\":" + str(round(max(d_lat[:,0]),2)) + ",\n\
-            \"lo2\":" + str(round(max(d_lon[0,:]),2)) + ",\n\
-            \"la2\":" + str(round(min(d_lat[:,0]),2)) + ",\n\
-            \"dx\":" + str(dx) + ",\n\
-            \"dy\":" + str(dy) + ",\n\
-        },\
-\n")
+    # V-component of surface wind
 
-outstr += str("        \"data\":[\n")
-for j in range(0,ny):
-    for i in range(0,nx):
-        if j != (ny-1) or i != (nx-1):
-            outstr += str("            " + str(d_v_10m_tr[j,i]) + ",\n")
-        else:
-            outstr += str("            " + str(d_v_10m_tr[j,i]) + "\n")
+    outstr += str("    {\n\
+            \"header\":{\n\
+                \"discipline\":0,\n\
+                \"disciplineName\":\"Meteorological products\",\n\
+                \"gribEdition\":2,\n\
+                \"gribLength\":791507,\n\
+                \"center\":8,\n\
+                \"centerName\":\"NCSU CNAPS\",\n\
+                \"subcenter\":0,\n\
+                \"refTime\":\"" + str(yyyy) + "-" + str(mm) + "-" + str(dd) + "T" + str(hh) +":00:00.000Z\",\n\
+                \"significanceOfRT\":1,\n\
+                \"significanceOfRTName\":\"Start of forecast\",\n\
+                \"productStatus\":0,\n\
+                \"productStatusName\":\"Operational products\",\n\
+                \"productType\":1,\n\
+                \"productTypeName\":\"Forecast products\",\n\
+                \"productDefinitionTemplate\":0,\n\
+                \"productDefinitionTemplateName\":\"Analysis/forecast at horizontal level/layer at a point in time\",\n\
+                \"parameterCategory\":2,\n\
+                \"parameterCategoryName\":\"Momentum\",\n\
+                \"parameterNumber\":3,\n\
+                \"parameterNumberName\":\"V-component_of_wind\",\n\
+                \"parameterUnit\":\"m.s-1\",\n\
+                \"genProcessType\":2,\n\
+                \"genProcessTypeName\":\"Forecast\",\n\
+                \"forecastTime\":0,\n\
+                \"surface1Type\":103,\n\
+                \"surface1TypeName\":\"Specified height level above ground\",\n\
+                \"surface1Value\":10.0,\n\
+                \"surface2Type\":255,\n\
+                \"surface2TypeName\":\"Missing\",\n\
+                \"surface2Value\":0.0,\n\
+                \"gridDefinitionTemplate\":0,\n\
+                \"gridDefinitionTemplateName\":\"Latitude_Longitude\",\n\
+                \"numberPoints\":" + str(points) +",\n\
+                \"shape\":6,\n\
+                \"shapeName\":\"Earth spherical with radius of 6,371,229.0 m\",\n\
+                \"gridUnits\":\"degrees\",\n\
+                \"resolution\":48,\n\
+                \"winds\":\"true\",\n\
+                \"scanMode\":0,\n\
+                \"nx\":" + str(nx) + ",\n\
+                \"ny\":" + str(ny) + ",\n\
+                \"basicAngle\":0,\n\
+                \"lo1\":" + str(round(min(d_lon[0,:]),2)) + ",\n\
+                \"la1\":" + str(round(max(d_lat[:,0]),2)) + ",\n\
+                \"lo2\":" + str(round(max(d_lon[0,:]),2)) + ",\n\
+                \"la2\":" + str(round(min(d_lat[:,0]),2)) + ",\n\
+                \"dx\":" + str(dx) + ",\n\
+                \"dy\":" + str(dy) + ",\n\
+            },\
+    \n")
 
-outstr += str("        ]\n\
-    }\n\
-]\n\
-")
+    outstr += str("        \"data\":[\n")
+    for j in range(0,ny):
+        for i in range(0,nx):
+            if j != (ny-1) or i != (nx-1):
+                outstr += str("            " + str(d_v_10m_tr[j,i]) + ",\n")
+            else:
+                outstr += str("            " + str(d_v_10m_tr[j,i]) + "\n")
 
-json_out.write(outstr)
-json_out.close()
+    outstr += str("        ]\n\
+        }\n\
+    ]\n\
+    ")
+
+    json_out.write(outstr)
+    json_out.close()
 
